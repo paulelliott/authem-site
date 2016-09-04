adf51016447e038dfcce126f924b258b8c2532fe
Paul Elliott <paul@codingfrontier.com>
2016-09-03 22:38:37 -0400

    Set up S3 sync

--- Gemfile
+++ Gemfile
@@ -3,5 +3,7 @@ ruby '2.3.1'
 #ruby-gemset=authem-site
 
 gem 'middleman', '~> 3.0'
+gem 'middleman-dotenv'
+gem 'middleman-s3_sync'
 gem 'middleman-syntax'
 gem 'redcarpet'
--- config.rb
+++ config.rb
@@ -2,6 +2,20 @@ set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
 
+set :markdown_engine, :redcarpet
+set :markdown, :fenced_code_blocks => true, :smartypants => true
+
+activate :asset_hash
+activate :dotenv
+activate :syntax
+
+activate :s3_sync do |s3_sync|
+  s3_sync.bucket = 'www.authem.org'
+  s3_sync.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
+  s3_sync.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
+  s3_sync.after_build = true
+end
+
 configure :build do
   activate :minify_css
   activate :minify_javascript
